68b81000b0221cece221fb23c9b51cc76500eae7
Natali Soares <natalisoares90@gmail.com>
2021-06-02 22:31:31 -0300

    sequencia de Fiboncci

---
+++ soma.php
@@ -0,0 +1,16 @@
+<?php
+
+$numeros = [1, 2, 3, 4, 5];      
+
+
+for ($cont = 0; $cont <= count($numeros); $cont++) {
+    
+     $resultado = $cont + $numeros[0]; 
+
+     echo $resultado . PHP_EOL;
+} 
+
+
+
+
+?>
